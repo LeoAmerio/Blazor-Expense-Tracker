235c4f5928ce2783504355927d2c3db753bc0e56
BA\lamerio <lamerio@tinsa.ar>
2023-08-22 10:00:58 -0300

    first configuration

---
+++ BlazorExpenseTracker.API/Program.cs
@@ -0,0 +1,28 @@
+using BlazorExpenseTracker.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//var SqlConnectionString = new SqlConfiguration(Configuration.GetConnectionString("SqlConnection"));
+//builder.Services.AddSingleton(SqlConnectionString);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ BlazorExpenseTracker.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,67 @@
+﻿using BlazorExpenseTracker.Model;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorExpenseTracker.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private SqlConfiguration _connectionString;
+
+        public CategoryRepository(SqlConfiguration connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected SqlConnection dbConnection()
+        {
+            return new SqlConnection(_connectionString.ConnectionString);
+        }
+
+        public Task<bool> DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategories()
+        {
+            var db = dbConnection();
+
+            var sql = @"SELECT Id, Name
+                        FROM Categories ";
+
+            return await db.QueryAsync<Category>(sql, new { });
+        }
+
+        public async Task<Category> GetCategoryDetails(int id)
+        {
+            var db = dbConnection();
+            var sql = @"SELECT Id, Name
+                        FROM Categories
+                        WHERE Id = @Id ";
+
+            return await db.QueryFirstOrDefaultAsync<Category>(sql, new { Id = id });
+        }
+
+        public async Task<bool> InsertCategory(Category category)
+        {
+            var db = dbConnection();
+            var sql = @"INSERT INTO Categories (Name)
+                        VALUES(@Name) ";
+
+            var result = db.ExecuteAsync(sql, new { category.Name });
+
+            return await result > 0;
+        }
+
+        public Task<bool> UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
